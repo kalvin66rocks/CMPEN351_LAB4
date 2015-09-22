80d61f65a30df3fbeb25472bf3798d4a0e0e9b47
pro585code <pro585g@gmail.com>
2015-09-22 08:38:20 -0400

    getInput

---
+++ Lab04Prgm1.asm
@@ -0,0 +1,22 @@
+# line break I got from stack overflow the rest is my code 
+.data
+
+var0:	    .word 0
+var1:	    .word 3
+
+strPrompt:  .asciiz "please enter a number " 
+
+.text
+
+GetInput:
+	#display prompt 
+	li $v0,4
+	la $a0, strPrompt	        # load address of prompt
+	syscall			        # print str prompt
+	li $v0,5 	 	        # take in input 
+	syscall
+	
+	la $t0,var0			# load address value to $t0
+	sw $v0,	0($t0)			# store $v0 into ram
+
+	addi $s0,$v0,0	                # load $v0 to $s0
